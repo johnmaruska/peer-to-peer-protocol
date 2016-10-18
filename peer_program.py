60d6574723d165b7db995fbcfdd9e2f12814dab3
John Maruska <jwmbq6@mst.edu>
2016-10-18 11:14:39 -0500

    Working GET and REQ LIST commands!

--- peer_program.py
+++ peer_program.py
@@ -1,153 +1,172 @@
-'''
+"""
     Peer Program for P2P File Transfer Protocol
     Handles both client and server portions of peer terminal.
-'''
+"""
 
 import socket
+import sys
 import threading
+import time
 import queue
 
-class PeerServer():
+
+class PeerServer:
     def __init__(self, host: str, port: int):
         self.host = host
         self.port = port
         # TODO: wrap these in try blocks ?
         self.welcome = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.welcome.setsockopt(SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.welcome.bind((self.host, self.port))
-    
+        self.welcome.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.welcome.bind((socket.gethostname(), self.port))
+
+    def listen_to_client(self, client):
+        try:
+            while True:
+                pass
+                # TODO: receive messages and send data
+                # P2P Protocol goes here.
+        except KeyboardInterrupt:
+            # client.close()
+            pass
+
     def listen(self):
-        self.welcome.listen(10) # 5 maximum clients connected to each Peer.
-        while True:
+        print("PeerServer listen() entered")  # TODO: Remove post-debug
+        self.welcome.listen(5)  # 5 maximum clients connected to each Peer.
+        accepting = True
+        while accepting:
             client, address = self.welcome.accept()
             # creates a new thread for each client that joins in
-            threading.Thread(target=listenToClient, args=(client,address)).start()
+            threading.Thread(target=self.listen_to_client, args=(client, address)).start()
             
     def quit(self):
         self.welcome.close()
-            
-    def listenToClient(self, client, address):
-        try:
-            while True:
-                # TODO: receive messages and send data
-                # P2P Protocol goes here.
-                
-        except KeyboardInterrupt:
-            client.close()
-            
+
     # TODO: implement send file as a server.
-    def send_file():
+    def send_file(self):
         pass
 
+
 def main():
-    this_host = 'localhost' # this system hostname
-    this_port = 8888 # this system port number
-    ts_host = ''
+    this_host = 'localhost'  # this system hostname
+    this_port = 8888  # this system port number
+    ts_host = '127.0.0.1'
     ts_port = 9999
     cmd_q = queue.Queue()
     ps = PeerServer(this_host, this_port)
-    
-    # Peer functions as a server for other peer-clients    
-    ps_t = threading.Thread(target=ps.listen).start()
-    # one thread for connection to the server
-    ts_t = threading.Thread(name='track', target=track_comm, args=(ts_host, ts_port, cmd_q)).start()
-    # one thread for raw input
-    in_t = threading.Thread(name='input', target=commands, args=(cmd_q)).start()
-        
-    while(ps_t.is_alive() && ts_t.is_alive() && in_t.is_alive()):
+    try:
+        # Peer functions as a server for other peer-clients
+        ps_t = threading.Thread(target=ps.listen)
+        ps_t.start()
+        # one thread for connection to the server
+        ts_t = threading.Thread(name='track', target=track_comm, args=(ts_host, ts_port, cmd_q))
+        ts_t.start()
+        # one thread for raw input
+        in_t = threading.Thread(name='input', target=commands, args=(cmd_q,))
+        in_t.start()
+    except RuntimeError:
+        print("ERROR: Could not start all threads.")
+
+    while ps_t.is_alive() and ts_t.is_alive() and in_t.is_alive():
         pass
     ps.quit()
-            
-def commands(cmd_q: Queue):
-    try: # loop input and enqueue command
-        while(True):
+
+
+def commands(cmd_q):
+    try:  # loop input and enqueue command
+        while True:
+            time.sleep(0.1)
             cmd = input('$ ')
             cmd_args = cmd.split(' ')
             
-            if(cmd_args[0] == 'createtracker'):
-                if(len(cmd_args) == 7):
+            if cmd_args[0] == 'createtracker':
+                if len(cmd_args) == 7:
                     cmd_q.put(cmd_args)
                 else:
-                    print("Improper number of arguments. Argument is formatted as: \
-                           createtracker [filename] [filesize] [description] [md5] \
-                           [ip-address] [port-number]")
-            elif(command_args[0] == 'updatetracker'):
-                if(len(cmd_args) == 6):
+                    print('Improper number of arguments. Argument is formatted as:' \
+                          'createtracker [filename] [filesize] [description] [md5]' \
+                          '[ip-address] [port-number]')
+            elif cmd_args[0] == 'updatetracker':
+                if len(cmd_args) == 6:
                     cmd_q.put(cmd_args)
                 else:
                     print("Improper number of arguments. Argument is formatted as: \
                            updatetracker [filename] [start_bytes] [end_bytes] \
                            [ip-address] [port-number]")
-            elif(command_args[0] == 'GET'):
-                if(len(cmd_args) == 2):
+            elif cmd_args[0] == 'GET':
+                if len(cmd_args) == 2:
                     cmd_q.put(cmd_args)
                 else:
                     print("Improper number of arguments. Argument is formatted as: \
                            GET [filename]")
-            elif(command_args[0] == 'REQ' and command_args[1] == 'LIST'):
-                if(len(cmd_args) == 2):
+            elif cmd_args[0] == 'REQ' and cmd_args[1] == 'LIST':
+                if len(cmd_args) == 2:
                     cmd_q.put(cmd_args)
                 else:
                     print("Improper number of arguments. REQ LIST has no arguments.")
     except KeyboardInterrupt:
         pass
 
-def cmd_tracker(server: socket.socket, cmd_q: Queue):
+
+def cmd_tracker(server, cmd_q):
     msg = ""
     next_cmd = cmd_q.get()
-    
-    if(next_cmd[0]=='createtracker'):
+    print(next_cmd)   # TODO: Remove post-debugging
+    if next_cmd[0] == 'createtracker':
         filename = next_cmd[1]
         filesize = next_cmd[2]
         desc = next_cmd[3]
         md5 = next_cmd[4]
         ip_addr = next_cmd[5]
         port_num = next_cmd[6]   
-        msg = "<createtracker %s %s %s %s %s %s>\n" % (filename, filesize, desc, md5, 
-                                                    ip_addr, port_num) 
-    elif(next_cmd[0]=='updatetracker'):
+        msg = "createtracker %s %s %s %s %s %s\n" % (filename, filesize, desc, md5,
+                                                     ip_addr, port_num)
+    elif next_cmd[0] == 'updatetracker':
         filename = next_cmd[1]
         start_bytes = next_cmd[2]
         end_bytes = next_cmd[3]
         ip_addr = next_cmd[4]
         port_num = next_cmd[5]
-        msg = "<updatetracker %s %s %s %s %s>\n" % (filename, start_bytes, 
+        msg = "updatetracker %s %s %s %s %s\n" % (filename, start_bytes,
                                                     end_bytes, ip_addr, port_num)
-    elif(next_cmd[0]=='GET'):
+    elif next_cmd[0] == 'GET':
         filename = next_cmd[1]
-        msg = "<GET %s>\n" % filename
-    elif(next_cmd[0]=='REQ' and next_cmd[1]=='LIST'):
-        msg = "<REQ LIST>\n"
+        msg = "GET %s\n" % filename
+    elif next_cmd[0] == 'REQ' and next_cmd[1] == 'LIST':
+        msg = "REQ LIST\n"
     
-    msg = msg + ";endTCPmessage"
+    msg += ";endTCPmessage"
     
-    if(len(msg) > 0):
-        server.sendall(msg)  
+    if len(msg) > 0:
+        msg = msg.encode("utf-8")
+        server.sendall(msg)
         
-        
-def track_comm(host: str, port: int, cmd_q: Queue):
+
+def track_comm(host: str, port: int, cmd_q: queue.Queue):
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.connect((host, port))
     try:
-        while(True):
+        while True:
             cmd_tracker(server, cmd_q)
             recv_from_tracker(server)
     except KeyboardInterrupt:
+        print("track_comm except KeyboardInterrupt entered")  # TODO: Remove post-debug
         server.close()
 
-# TODO: Check to see if ;endTCPmessage is being removed from total_msg
+
 def recv_from_tracker(server: socket.socket):
+    print("recv_from_tracker entered")  # TODO: Remove post-debug
     end_marker = ";endTCPmessage"
-    total_msg=[]
-    msg=''
+    total_msg = []
     while True:
-        msg = server.recv(1024)
+        msg = (server.recv(1024)).decode("utf-8")
         if end_marker in msg:
             total_msg.append(msg[:msg.find(end_marker)])
             break
         total_msg.append(msg)
-        if len(total_msg) > 1: # TODO: This section weirds me out. It's supposed to handle
-                               # what happens on a split msg, but I'm not sure how it works.
+
+        """TODO: This section weirds me out. It's supposed to handle what happens on a split msg, but I'm not sure how
+        it works."""
+        if len(total_msg) > 1:
             # check if end of msg was split
             last_pair = total_msg[-2]+total_msg[-1]
             if end_marker in last_pair:
@@ -155,6 +174,7 @@ def recv_from_tracker(server: socket.socket):
                 total_msg.pop()
                 break
     print(''.join(total_msg))
+    print('recv_from_tracker exited')  # TODO: Remove post-debugging
         
-if __name__=="__main__":
+if __name__ == "__main__":
     sys.exit(main())
--- tracking_server.py
+++ tracking_server.py
@@ -1,165 +1,193 @@
-'''
+"""
     Tracking Server Program for P2P File Transfer Protocol
-'''
+"""
 
 import glob
 import os
-import queue
 import re
 import socket
+import sys
 import threading
 import time
 
-# TODO: Fix message structure to include '<' '>' '\n' characters.
 
+# TODO: Fix message structure to include '<' '>' '\n' characters.
 def main():
-    host = ''
+    host = 'localhost'
     port = 9999
     welcome = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    welcome.setsockopt(SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    welcome.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     welcome.bind((host, port))
-    
+    listen(welcome)
+
+
 def listen(welcome: socket.socket):
-    welcome.listen(5) # 10 maximum clients connected to server.
+    welcome.listen(5)  # 5 maximum clients connected to server.
     try:
         # create a thread for each peer client that wants to interact with server
         while True:
             client, address = welcome.accept()
-            threading.Thread(target=listenToClient, args=(client,address)).start()
-    finally
+            threading.Thread(target=listen_to_client, args=(client,)).start()
+    finally:
         welcome.close()
 
-def listenToClient(client, address):
+
+def listen_to_client(client):
     try:
         while True:
             # receive command
             cmd_in = recv_msg(client)
-            
             # Check for and execute createtracker command
             if re.match("createtracker *", cmd_in):
+                print("cmd matched createtracker")
                 try:
                     # grab the arguments of the createtracker command.
                     # ("*") allows description to contain spaces; others space limited
                     m = re.match('(createtracker) ([^ ]+) ([^ ]+) ("*") ([^ ]+) ([^ ]+)'
                                  ' ([^ ]+)', cmd_in)
-                    f_name = m.group(2); f_size = m.group(3)
-                    desc = m.group(4); md5 = m.group(5)
-                    ip_addr = m.group(6); port_num = m.group(7)
-                    reply_out = createtracker(f_name, f_size, desc, md5, ip_addr, port_num)
-                    client.send(reply_out)
-                except IndexError, AttributeError: 
-                    reply_out = '<createtracker fail>\n'
-                    client.send(reply_out)
-            
-            # Check for and execute updatetracker command
-            elif re.match('updatetracker *', cmd_in):
-                try:
-                    m = re.match('(updatetracker) ([^ ]+) ([^ ]+) ([^ ]+) ([^ ]+) ([^ ]+)')
                     f_name = m.group(2)
-                    start_bytes = m.group(3); end_bytes = m.group(4)
-                    ip_addr = m.group(5); port_num = m.group(6)
+                    f_size = m.group(3)
+                    desc = m.group(4)
+                    md5 = m.group(5)
+                    ip_addr = m.group(6)
+                    port_num = m.group(7)
+                    reply_out = createtracker(f_name, f_size, desc, md5, ip_addr, port_num)
+                    print(reply_out)  # TODO: Remove post-debugging
+                    client.send(reply_out.encode('utf-8'))
+                except (IndexError, AttributeError):
+                    reply_out = 'createtracker fail\n'
+                    client.send(reply_out.encode('utf-8'))
+            # Check for and execute updatetracker command
+            if re.match('updatetracker *', cmd_in):
+                print("cmd matched updatetracker")
+                try:
+                    m = re.match('(updatetracker) ([^ ]+) ([^ ]+) ([^ ]+) ([^ ]+) ([^ ]+)', cmd_in)
+                    f_name = m.group(2)
+                    start_bytes = m.group(3)
+                    end_bytes = m.group(4)
+                    ip_addr = m.group(5)
+                    port_num = m.group(6)
                     reply_out = updatetracker(f_name, start_bytes, end_bytes, ip_addr, port_num)
-                    client.send(reply_out)
-                except IndexError, AttributeError:
-                    reply_out = '<updatetracker fail>\n'
-                    client.send(reply_out)           
-            
+                    client.send(reply_out.encode('utf-8'))
+                except (IndexError, AttributeError):
+                    reply_out = 'updatetracker fail\n'
+                    client.send(reply_out.encode('utf-8'))
             # Check for and execute REQ LIST command
-            elif cmd_in == "REQ LIST":
+            if cmd_in == 'REQ LIST\n':
+                print("cmd_in matched REQ_LIST")
                 reply_out = req_list()
-                client.send(reply_out)
-            
+                client.send(reply_out.encode('utf-8'))
             # Check for and execute GET command
-            elif cmd_in == "GET *", cmd_in):
-                m = re.match("GET ([^ ]+\.track)")
+            if re.match('GET .*\\n$', cmd_in):
+                print("cmd matched GET")
+                m = re.match('GET ([^ ]+\.track)', cmd_in)
+                print(m.group(1))
                 try:
                     tracker_file = m.group(1)
                     reply_out = get(tracker_file)
                 except AttributeError:
                     reply_out = cmd_in + ' does not request a .track file.'
+                reply_out += ';endTCPmessage'
+                client.send(reply_out.encode('utf-8'))
 
     except KeyboardInterrupt:
         client.close()
-            
-def get(tracker): -> str
-    reply = '<REP GET BEGIN>\n<'
-    with open(tracker, 'r') as f:
-        for line in f:
-            reply.append(line)
-            if "MD5: " in line:
-                md5 = line.split(' ')[1]
-    reply.append('>\nREP GET END %s>\n' % md5)
+
+
+def get(tracker):
+    reply = 'REP GET BEGIN\n'
+    md5 = ''
+    try:
+        with open(tracker, 'rt') as f:
+            for line in f:
+                reply += line
+                if "MD5: " in line:
+                    md5 = line.split(' ')[1]
+        reply += '\nREP GET END'
+        if md5 != '':
+            reply += (' %s\n' % md5)
+    except FileNotFoundError:
+        reply = 'ERROR: Could not GET. File %s not found.' % tracker
     return reply
-            
-def req_list(): -> str
+
+
+def req_list():
     # os.chdir("/mydir") #mydir would have to be changed
     track_files = glob.glob('./*.track')
-    reply_msg = '<REP LIST %s>\n' % len(track_files)
-    for file in range(len(track_files)):
-        # read file
-        with open(track_files[file], 'r') as f:
+    reply_msg = 'REP LIST %s\n' % len(track_files)
+    file_num = 0
+    for t_file in range(len(track_files)):
+        file_num += 1
+        with open(track_files[t_file], 'rt') as f:
             for line in f:
                 if "Filename: " in line:
-                    name = line.split(' ')[1]
+                    name = line.split(' ')[1].strip('\n')
                 elif "Filesize: " in line:
-                    size = line.split(' ')[1]
+                    size = line.split(' ')[1].strip('\n')
                 elif "MD5: " in line:
-                    md5 = line.split(' ')[1]
-        reply_msg.append('<%s %s %s %s>\n' %(f, name, size, md5))
-    reply_msg.append('<REP LIST END>\n')
+                    md5 = line.split(' ')[1].strip('\n')
+        try:
+            reply_msg += ('%s %s %s %s\n' % (file_num, name, size, md5))
+        except UnboundLocalError:
+            reply_msg = '%s ERROR: %s not properly formatted.\n' % (file_num, track_files[t_file])
+            pass
+    reply_msg += 'REP LIST END\n;endTCPmessage'
+    print(reply_msg)  # TODO: Remove post-debug
     return reply_msg
-        
-def updatetracker(f_name, start_byte, end_byte, ip_addr, port_num): -> str
-    '''
+
+
+def updatetracker(f_name, start_byte, end_byte, ip_addr, port_num):
+    """
         Does the bulk of the updatetracker command. i.e. modifying the tracker file.
         Returns the reply string to send back to the client.
-    '''
+    """
     # Ensure the file name ends with '.track' extension.
-    if re.match('\.[\w]+\Z',f_name):
-        if not re.match('\.track\Z', f_name):
-            f_track = re.sub('\.[\w]+\Z', '.track', f_name)
+    if re.match('[^ ]*\.[\w]+\Z', f_name):
+        if not re.match('[^ ]+\.track\Z', f_name):
+            f_track = re.sub("\.[\w]+\Z", '.track', f_name)
         else:
             f_track = f_name
-    else
-        f_track = f_track + ".track"
+    else:
+        f_track = f_name + ".track"
     # Modify and rewrite file with new information.
     if os.path.isfile('./%s' % f_track):
         try:
-            with open(f_track, 'r') as f:
+            with open(f_track, 'rt') as f:
                 prev_contents = f.read()
             old_pattern = '%s:%s:[^:]+:[^:]:[\w]+' % (ip_addr, port_num)
             timestamp = time.time()
             new_line = '%s:%s:%s:%s:%s' % (ip_addr, port_num, start_byte, end_byte, timestamp)
-            new_contents = re.sub(pattern, new_line, prev_contents)
-            with open(f_track, 'w') as f
+            new_contents = re.sub(old_pattern, new_line, prev_contents)
+            with open(f_track, 'wt') as f:
                 f.write(new_contents)
-            reply_out = '<updatetracker succ>\n'
-        except:
-            reply_out = '<updatetracker fail>\n'
+            reply_out = 'updatetracker succ\n'
+        except FileNotFoundError:
+            reply_out = 'updatetracker fail\n'
     else:
-        reply_out = '<updatetracker ferr>\n'
-    
+        reply_out = 'updatetracker ferr\n'
+
     return reply_out
-        
-def createtracker(f_name, f_size, desc, md5, ip_addr, port_num): -> str
-    ''' 
-        Does the bulk of the createtracker command, i.e. create the tracker file 
+
+
+def createtracker(f_name, f_size, desc, md5, ip_addr, port_num):
+    """
+        Does the bulk of the createtracker command, i.e. create the tracker file
         Filename cannot contain any '.' characters that don't prefix the file extension.
         Returns the reply string to send back to the client.
-    '''
+    """
     if os.path.isfile('./%s' % f_name):
         # File already exists
-        reply_out = '<createtracker ferr>\n'
-    else
+        reply_out = 'createtracker ferr\n'
+    else:
         # Changes file extension to .track
-        if re.match('\.[\w]+\Z'):
+        if re.match('\.[\w]+\Z', f_name):
             if not re.match('\.track\Z', f_name):
                 f_track = re.sub('\.[\w]+\Z', '.track', f_name)
-            else: # file already has .track extension
+            else:  # file already has .track extension
                 f_track = f_name
-        else # appends .track if no given file extension
-            f_track = f_track + ".track"
-        with open(f_track, 'w') as f
+        else:  # appends .track if no given file extension
+            f_track = f_name + ".track"
+        with open(f_track, 'wt') as f:
             f.write('Filename: %s\n' % f_track)
             f.write('Filesize: %s\n' % f_size)
             f.write('Description: %s\n' % desc)
@@ -168,25 +196,32 @@ def createtracker(f_name, f_size, desc, md5, ip_addr, port_num): -> str
                     '# following the above fields about file to be shared will be list of peers'
                     'sharing this file\n')
             timestamp = time.time()
-            f.write('%s:%s:0:%s:%s' % (ip_addr, port_num, file_size,timestamp)
-        reply_out = '<createtracker succ>\n'
+            f.write('%s:%s:0:%s:%s' % (ip_addr, port_num, f_size, timestamp))
+        reply_out = 'createtracker succ\n'
     return reply_out
-    
-def recv_msg(client: socket.socket): -> str
+
+
+def recv_msg(client: socket.socket):
     end_marker = ";endTCPmessage"
-    total_msg=[]
-    msg=''
+    total_msg = []
     while True:
-        msg = client.recv(1024)
-        if end_marker in msg:
-            total_msg.append(msg[:msg.find(end_marker)])
-            break
+        try:
+            msg = (client.recv(1024)).decode("utf-8")
+            if end_marker in msg:
+                total_msg.append(msg[:msg.find(end_marker)])
+                break
+        except TypeError:
+            print(type(msg))
+            print(type(end_marker))
         total_msg.append(msg)
         if len(total_msg) > 1:
             # check if end of msg was split
-            last_pair = total_msg[-2]+total_msg[-1]
+            last_pair = total_msg[-2] + total_msg[-1]
             if end_marker in last_pair:
                 total_msg[-2] = last_pair[:last_pair.find(end_marker)]
                 total_msg.pop()
                 break
     return ''.join(total_msg)
+
+if __name__ == "__main__":
+    sys.exit(main())
