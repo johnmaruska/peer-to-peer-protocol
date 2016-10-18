abedc8bee5491d694e3fc752bc8bfe47ef9aa56b
NEUZhangy <alice7neusoft1207@gmail.com>
2016-10-17 23:56:26 -0500

    Change socketserver to low-level socket

--- file_spliter.py
+++ file_spliter.py
@@ -1,9 +1,11 @@
-import subprocess, os
+import os
+import subprocess
+
 #Total command would be "split -n [number_of_file] input.pdf ./temp/
 
 #def split_file(self, filename, number_of_file):
-command = "split -n"
-number_of_file = 10
+command = "split -b"
+number_of_file = 10**5
 command += " " + str(number_of_file)
 filename = "input.pdf"
 suffix = "./temp/"
@@ -11,5 +13,3 @@ command += " " + filename + " " + suffix
 p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 file = os.listdir('./temp')
 os.chdir("./temp")
-p = subprocess.Popen("cat * >> input.pdf", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-print (file)
--- sendfile_server.py
+++ sendfile_server.py
@@ -1,70 +1,73 @@
-import SocketServer,os
+import os
+import socket
+import subprocess
 
-class MyTCPHandler(SocketServer.BaseRequestHandler):
-    """
-    The request handler class for our server.
 
-    It is instantiated once per connection to the server, and must
-    override the handle() method to implement communication to the
-    client.
-    """
+#Rewrite the whole socketserver
+
+def split_file(self, filename, number_of_file):
+    #For macOS, it doesn't have command for split -n, use split -b instead for testing
+    command = "split -b"
+    number_of_file = 10**6
+    command += " " + str(number_of_file)
+    filename = "input.pdf"
+    suffix = "./temp/"
+    command += " " + filename + " " + suffix
+    p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    filelist = os.listdir('./temp')
+    os.chdir("./temp")
+    #p = subprocess.Popen("cat * >> input.pdf", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    #print (file)
+    return filelist
+
+def handle(self):
+    # self.request is the TCP socket connected to the client
+    # Send file when client connects
+    '''
+    Server send the segment length at very first.
+        SEGMENT [numbers of segment]
+    Client should ask the segment first, or return the segment number it received last time.
+    And the format should be:
+        SUCCESS [number]
+        REQUEST [number]
+        FINISH
 
     '''
-    Who knows?
-    Don't read the whole file to the memory
-    Try seek function to read specify byte
-    Client needs to assemble it.
-    '''
-    tracker = []
+    # split file
+    print("split files")
+    filelist = self.split_file("input.pdf", 10)
+    segment_number = len(filelist)
+    msg = "SEGMENT " + str(segment_number)
+    self.request.sendall(msg)
+    res = ""
+    res = self.request.recv(1024)
+    print res
+    nth_segment = 0
+    # split the file & Send
+    while True:
+        if res == "FINISH":
+            break
+        if res.split(' ')[0] == "REQUEST":
+            print res
+            res_list = res.split(' ')
+            print (int(res_list[1]))
+            print ("Send File" + res_list[1])
+            output = open(filelist[int(res_list[1])], "rb")
+            self.request.send(output.read())
+            res = self.request.recv(1024)
+            print (res)
+            output.close()
 
-    def handle(self):
-        nth_segment = 0  # the nth segment
-        # self.request is the TCP socket connected to the client
-        # Send file when client connects
-        filename = "input.pdf"
-        f = open(filename, "rb")
-        size = os.path.getsize(filename)
-        print size
-        segment_length = size / 100
-        '''
-        Server send the segment length at very first.
-            SEGMENT [size]
-        Client should ask the segment first, or return the segment number it received last time.
-        And the format should be:
-            SUCCESS [number]
-            REQUEST [number]
-            FINISH
-
-        '''
-        msg = "SEGMENT " + str(segment_length)
-        print msg
-        self.request.sendall(msg)
-        res = ""
-        res = self.request.recv(1024)
-        # divide the file & Send
-        while True:
-            if res == "FINISH":
-                break
-            else:
-                res_list = res.split(' ')
-                if res_list == "SUCCESS":
-                    nth_segment = int(res_list[1])
-                elif res_list == "REQUEST":
-                    nth_segment = int(res_list[1])
-                print "Send File" + str(nth_segment)
-                f.seek(nth_segment * segment_length, 0)
-                send_segment = f.read(segment_length)
-                self.request.sendall(send_segment)
-                res = self.request.recv(1024)
-        
 
 
 if __name__ == "__main__":
-    HOST, PORT = "localhost", 9999
+    HOST, PORT = "localhost", 30000
 
     # Create the server, binding to localhost on port 9999
-    server = SocketServer.TCPServer((HOST, PORT), MyTCPHandler)
+    server = socket.socket()
+    server.connect((HOST, PORT))
+    server.listen(10)
+    clientsocket,addr = server.accept()
+    print("Got a connection from %s" % str(addr))
 
-    # Activate the server; this will keep running until you
-    # interrupt the program with Ctrl-C
-    server.serve_forever()
+
